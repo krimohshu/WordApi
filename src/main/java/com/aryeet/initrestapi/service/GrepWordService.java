c7f26bfb27ebacc65bf272a52a0f99b30acb1910
krish shukla <krishan.shyam@gmail.com>
2020-03-23 23:26:50 +0000

    modified both method with same comparator

--- src/main/java/com/aryeet/initrestapi/service/GrepWordService.java
+++ src/main/java/com/aryeet/initrestapi/service/GrepWordService.java
@@ -21,30 +21,13 @@ public class GrepWordService {
         Map<String, Integer> wordMap = new HashMap<>();
 
         if (sentence != null) {
-            String[] currentWord = sentence.split(" ");
-            String longword = " ";
 
-            for (int i = 0; i < currentWord.length; i++) {
+            Optional<String> shortest = Arrays.asList(sentence.split(" ")).stream()
+                    .sorted(byLength).findFirst();
 
-                if (currentWord[i].length() >= longword.length()) {
+            wordMap.put(shortest.get(), shortest.get().length());
 
-                    longword = currentWord[i];
-                }
-            }
-            wordMap.put(longword, longword.length());
-        }
-
-        /*if (sentence != null) {
-            Set<String> setOfSmallestNumber = Arrays.asList(sentence.split(" "))
-                    .stream()
-                    .sorted((e2, e1) -> e1.length()  e2.length() ? -1 : 1)
-                    .collect(Collectors.toSet());
-
-            setOfSmallestNumber.stream().forEach(word -> {
-                wordMap.put(word, word.length());
-            });
-        }*/
-        else {
+        } else {
             wordMap.put("Please provide the non-null Input", 0);
         }
 
@@ -60,31 +43,14 @@ public class GrepWordService {
     public Map<String, Integer> getShortestWord(String sentence) {
 
         Map<String, Integer> wordMap = new HashMap<>();
-        if(sentence !=null) {
+        if (sentence != null) {
             Optional<String> shortest = Arrays.asList(sentence.split(" ")).stream()
                     .sorted(byLength.reversed()).findFirst();
 
             wordMap.put(shortest.get(), shortest.get().length());
-        }
-        else{
-            wordMap.put("Please provide the non-null Input", 0);
-        }
-
-
-
-     /*   if (sentence != null) {
-            List<String> setOfSmallestNumber = Arrays.asList(sentence.split(" "))
-                    .stream()
-                    .sorted((e2, e1) -> e1.length() > e2.length() ? -1 : 1)
-                    .collect(Collectors.toList());
-
-            setOfSmallestNumber.stream().forEach(word -> {
-                wordMap.put(word, word.length());
-            });
         } else {
             wordMap.put("Please provide the non-null Input", 0);
-        }*/
-
+        }
         return wordMap;
     }
 
